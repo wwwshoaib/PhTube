e7df91e4b201d0b07ad9af9ed4e587279b62648c
Shoaib <shoaibchemistrysust@gmail.com>
2024-12-20 20:03:15 +0600

    videos loader added

--- js/video.js
+++ js/video.js
@@ -1,17 +1,17 @@
 //load categories
 const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res => res.json())
-    .then(data => displayCategories(data.categories))
-    .catch((error) => console.log(error) )
-}; 
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories))
+        .catch((error) => console.log(error))
+};
 
 //load videos
 const loadVideos = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res => res.json())
-    .then(data => displayCategories(data.categories))
-    .catch((error) => console.log(error) )
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch((error) => console.log(error))
 };
 
 //displayCategories
@@ -35,4 +35,35 @@ const displayCategories = (categories) => {
     })
 }
 
+//display videos 
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos')
+    //categories are now in category
+    videos.forEach((video) => {
+        //create a category button
+        const card = document.createElement('div');
+        card.classList = 'card card-compact bg-base-100';
+        card.innerHTML = `
+        <figure>
+            <img
+            src="${video.thumbnail}"
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+        videoContainer.append(card);
+    })
+}
+
+
+//call functions
+
 loadCategories();
+loadVideos();
